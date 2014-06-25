ac05f42f22f24e30e6b01d375e3fd712cbf3b621
Oliver Davenport <oliver@oliverdavenport.co.uk>
2014-06-25 17:30:31 +0100

    Replace __autoload() with direct require_once() calls

--- src/v1/v1.php
+++ src/v1/v1.php
@@ -1,10 +1,26 @@
 <?php
-function __autoload($class) {
+session_start();
+
+/*function __autoload($class) {
+	print(__FILE__);
 	$path = realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR.$class.".php";
 
-	if(!class_exists($class) && file_exists($path))
+	if(!class_exists($class) && file_exists($path)) {
+		printf(" REQUIRING: %s\n", $path);
 		require_once($path);
-}
+	}
+}*/
+
+require_once("Code.php");
+require_once("Color.php");
+require_once("Cube.php");
+require_once("CURL.php");
+require_once("DB.php");
+require_once("Entity.php");
+require_once("Light_Mode.php");
+require_once("PushWeather.php");
+require_once("User.php");
+require_once("User_Service.php");
 
 // Mysql Defines
 define("MYSQL_HOSTNAME", "localhost");
